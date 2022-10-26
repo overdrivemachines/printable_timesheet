882cbf6711f33e8c15fc6e4e09be2244d1b261a6
Dipen Chauhan <get.dipen@gmail.com>
2022-10-25 21:28:43 -0700

    setting timesheet body

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Live - https://overdrivemachines.github.io/printable_timesheet/
 
 ## References
 - Get Number of days in a month - https://bobbyhadz.com/blog/javascript-get-number-of-days-in-month
+- Date - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const caption1El = document.querySelector(".timesheet-caption-1");
-const caption2El = document.querySelector(".timesheet-caption-2");
-
 //////////////////////////////////////////////////////////////////
 // Returns the number of days in a month
 // month is in the range [0..11]
@@ -11,11 +8,31 @@ function getDaysInMonth(year, month) {
   return new Date(year, month + 1, 0).getDate();
 }
 
+//////////////////////////////////////////////////////////////////
+// Returns the ordinal suffix of a number
+// n can be any number
+//////////////////////////////////////////////////////////////////
+function getOrdinal(n) {
+  let ord = 'th';
+
+  if (n % 10 == 1 && n % 100 != 11) {
+    ord = 'st';
+  } else if (n % 10 == 2 && n % 100 != 12) {
+    ord = 'nd';
+  } else if (n % 10 == 3 && n % 100 != 13) {
+    ord = 'rd';
+  }
+  return ord;
+}
+
 
 //////////////////////////////////////////////////////////////////
 // Set Captions for both timesheets
 //////////////////////////////////////////////////////////////////
 function setCaptions() {
+  const caption1El = document.querySelector(".timesheet-caption-1");
+  const caption2El = document.querySelector(".timesheet-caption-2");
+
   // get the month name
   const monthName = date.toLocaleString('default', { month: 'long' });
   
@@ -29,14 +46,51 @@ function setCaptions() {
 //////////////////////////////////////////////////////////////////
 // Set Timesheet Body
 //////////////////////////////////////////////////////////////////
+function setTimesheetBody() {
+  const timesheetBody1El = document.querySelector(".timesheet-body-1");
+  const timesheetBody2El = document.querySelector(".timesheet-body-2");
+
+  // set a temporary date that will change from 1st to 31st in the loop
+  let tempDate = new Date(date);
+  tempDate.setDate(1);
+  
+  // Timesheet1
+  for (let i = 1; i <= daysIntimesheetMonth; i++) {
+    tempDate.setDate(i);
+    const tr = document.createElement("tr");
+    const tdDate = document.createElement("td");
+    tdDate.textContent = i + getOrdinal(i);
+    const tdDay = document.createElement("td");
+    tdDay.textContent = tempDate.toLocaleDateString('en-us', { weekday: 'short' });
+    const tdBlank = document.createElement("td");
+
+    tr.appendChild(tdDate);
+    tr.appendChild(tdDay);
+    tr.appendChild(document.createElement("td"));
+    tr.appendChild(document.createElement("td"));
+    tr.appendChild(document.createElement("td"));
+    tr.appendChild(document.createElement("td"));
+    tr.appendChild(document.createElement("td"));
+
+    if (i <= 15) {
+      timesheetBody1El.appendChild(tr);
+    } else {
+      timesheetBody2El.appendChild(tr);
+    }
+    
+  }
+  
+  
+}
 
 //////////////////////////////////////////////////////////////////
 // On Load...
 const date = new Date();
+date.setDate(date.getDate() + 5);
 const timesheetYear = date.getFullYear();
 const timesheetMonth = date.getMonth(); // months are 0-based
 const daysIntimesheetMonth = getDaysInMonth(timesheetYear, timesheetMonth);
 
 
 setCaptions();
-
+setTimesheetBody();
