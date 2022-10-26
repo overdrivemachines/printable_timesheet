2a690dc214b128bce6334fe3413e610f70240a26
Dipen Chauhan <get.dipen@gmail.com>
2022-10-25 20:40:18 -0700

    set timesheet captions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# printable_timesheet
+# Printable Timesheet
 Make a Printable Timesheet using JavaScript
+
+![Preview](preview.png)
+
+Live - https://overdrivemachines.github.io/printable_timesheet/
+
+## References
+- Get Number of days in a month - https://bobbyhadz.com/blog/javascript-get-number-of-days-in-month
--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 const caption1El = document.querySelector(".timesheet-caption-1");
 const caption2El = document.querySelector(".timesheet-caption-2");
 
+//////////////////////////////////////////////////////////////////
+// Returns the number of days in a month
+// month is in the range [0..11]
+//////////////////////////////////////////////////////////////////
+function getDaysInMonth(year, month) {
+  // +1 added to month because .getDate() returns the number of
+  // days in the previous when day is set to 0
+  return new Date(year, month + 1, 0).getDate();
+}
+
+
+//////////////////////////////////////////////////////////////////
+// Set Captions for both timesheets
+//////////////////////////////////////////////////////////////////
+function setCaptions() {
+  // get the month name
+  const monthName = date.toLocaleString('default', { month: 'long' });
+  
+  // Set Timesheet 1's caption
+  caption1El.textContent = monthName + " 1 - 15, " + timesheetYear;
+
+  // Set Timesheet 2's caption
+  caption2El.textContent = monthName + " 16 - " + getDaysInMonth(timesheetYear, timesheetMonth) + ", " + timesheetYear;
+}
+
+//////////////////////////////////////////////////////////////////
+// Set Timesheet Body
+//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+// On Load...
+const date = new Date();
+const timesheetYear = date.getFullYear();
+const timesheetMonth = date.getMonth(); // months are 0-based
+const daysIntimesheetMonth = getDaysInMonth(timesheetYear, timesheetMonth);
+
+
+setCaptions();
+
